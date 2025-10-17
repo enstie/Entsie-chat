1fc1ae391fe00ab83bc9d6c6162329360a18a528
Justice Entsie <150168172+enstie@users.noreply.github.com>
2025-10-17 10:06:51 +0000

    Create NextAuth API route

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,5 @@
+import NextAuth from 'next-auth';
+import { authOptions } from '@/lib/auth';
+
+export const GET = (req, res) => NextAuth(req, res, authOptions);
+export const POST = (req, res) => NextAuth(req, res, authOptions);
